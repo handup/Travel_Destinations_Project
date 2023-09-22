ca32a119d861c78e68fa5d3d7849ab0f86f459a6
Pavel Andrei <pavelhandup@gmail.com>
2023-09-22 19:09:08 +0300

    made post with database work

--- TravelDestinationServerSide/server.js
+++ TravelDestinationServerSide/server.js
@@ -1,35 +1,58 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const express = require('express');
 const app = express();
 const cors = require('cors'); // Import the 'cors' package
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+const hostname = '127.0.0.1';
+const uri = `mongodb://${hostname}:27017`;
 
-  app.use(cors({
-    origin: '*', // Replace '*' with the actual origin(s) in a production environment
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  }));
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+app.use(cors({
+  origin: '*', // Replace '*' with the actual origin(s) in a production environment
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
 
 // GET localhost:40000/destinations => return all destinations
-app.get('/destinations', (req, res) => {    
-    res.send(destinations);
+app.get('/destinations', async (req, res) => {
+
+  const db = client.db("travel_destination")
+  const tdCollection = db.collection("destinations")
+  const result = await tdCollection.find().toArray()
+  res.send(result);
 });
 
 // POST localhost:40000/destinations => add a new destination
-app.post('/destinations', (req, res) => {
-    const newDestination = req.body;
-    destinations.push(newDestination);
-    res.send(newDestination);
+app.post('/destinations', async (req, res) => {
+  const newDestination = req.body;
+
+  const db = client.db("travel_destination")
+  const tdCollection = db.collection("destinations")
+  const result = await tdCollection.insertOne(newDestination)
+  res.status(201).send(`A document was inserted with the _id: ${result.insertedId}`);
 });
 
 
 
-const destinations = [ {
+/* const destinations = [ {
 country: "Italy",
 link: "https://www.lonelyplanet.com/italy",
 title: "Italy",
@@ -37,4 +60,4 @@ description: "Italy is a country located in Southern Europe comprising the boot-
 arrivalDate: "2018-04-01",
 departureDate: "2018-04-14",
 // image: "https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/Italy/italy-attractions.jpg?imwidth=450"
-    }]  
+    }]   */
