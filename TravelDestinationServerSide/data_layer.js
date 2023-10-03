1f361ce48edf033067a0256a0b3a7e45dccda563
Pavel Andrei <pavelhandup@gmail.com>
2023-10-03 19:32:54 +0300

    setupmongoose and refactored stuff hell yeah

---
+++ TravelDestinationServerSide/data_layer.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+
+const destinationSchema = new mongoose.Schema({
+  country: String,
+  link: String,
+  title: String,
+  description: String,
+  arrivalDate: String,
+  departureDate: String,
+  image: String,
+});
+
+
+
+const hostname = '127.0.0.1';
+const uri = `mongodb://${hostname}:27017`;
+
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+  
+export const Destination = mongoose.model('destinations', destinationSchema);
+export const getDestinations = () => Destination.find()
+export const createDestination = (newDestination) => new Destination(...newDestination).save()
--- TravelDestinationServerSide/server.js
+++ TravelDestinationServerSide/server.js
@@ -1,25 +1,11 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+import { getDestinations, createDestination } from './data_layer.js';
+import express from 'express'
+import cors from 'cors'
 
-const express = require('express');
 const app = express();
-const cors = require('cors'); // Import the 'cors' package
 
 const port = 3000;
-
-const hostname = '127.0.0.1';
-const uri = `mongodb://${hostname}:27017`;
-
 app.use(express.json({ limit: '1000Gb' }))
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
@@ -34,20 +20,10 @@ app.use(cors({
 
 
 // GET localhost:40000/destinations => return all destinations
-app.get('/destinations', async (req, res) => {
-
-  const db = client.db("travel_destination")
-  const tdCollection = db.collection("destinations")
-  const result = await tdCollection.find().toArray()
-  res.send(result);
-});
+app.get('/destinations', async (req, res) => await getDestinations().then(result => res.send(result)).catch(err=>console.log(err)));
 
 // POST localhost:40000/destinations => add a new destination
-app.post('/destinations', async (req, res) => {
-  const newDestination = req.body;
-
-  const db = client.db("travel_destination")
-  const tdCollection = db.collection("destinations")
-  const result = await tdCollection.insertOne(newDestination)
-  res.status(201).send(`A document was inserted with the _id: ${result.insertedId}`);
-});
+app.post('/destinations', async (req, res) =>
+  await createDestination(req.body)
+  .then(result => res.status(201).send(`A document was inserted with the _id: ${result.insertedId}`))
+  .catch(err=> console.log(err)))
