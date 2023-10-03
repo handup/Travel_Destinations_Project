0c5e838f2c66e207a9e4f0b9077277ff7d10c899
Pavel Andrei <pavelhandup@gmail.com>
2023-10-03 19:33:33 +0300

    whitespacing

--- TravelDestinationServerSide/data_layer.js
+++ TravelDestinationServerSide/data_layer.js
@@ -1,21 +1,5 @@
 import mongoose from "mongoose"
 
-const destinationSchema = new mongoose.Schema({
-  country: String,
-  link: String,
-  title: String,
-  description: String,
-  arrivalDate: String,
-  departureDate: String,
-  image: String,
-});
-
-
-
-const hostname = '127.0.0.1';
-const uri = `mongodb://${hostname}:27017`;
-
-
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,6 +10,20 @@ mongoose.connect(uri, {
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
   });
+
+const destinationSchema = new mongoose.Schema({
+  country: String,
+  link: String,
+  title: String,
+  description: String,
+  arrivalDate: String,
+  departureDate: String,
+  image: String,
+});
+
+const hostname = '127.0.0.1';
+const uri = `mongodb://${hostname}:27017`;
+
   
 export const Destination = mongoose.model('destinations', destinationSchema);
 export const getDestinations = () => Destination.find()
