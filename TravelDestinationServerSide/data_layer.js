d683dca7805b8001c44687356f9063d2a0352ee0
ThisIsTheNewOne <baltapaul2@gmail.com>
2023-10-05 18:26:45 +0200

    adding the template  and the delete and update

--- TravelDestinationServerSide/data_layer.js
+++ TravelDestinationServerSide/data_layer.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose"
 
 
 const hostname = '127.0.0.1';
-const uri = `mongodb://${hostname}:27017`;
+// by default it will go to test database and to avoid it I specified the name of the database
+const dbName = 'travel_destinations';
+const uri = `mongodb://${hostname}:27017/${dbName}`;
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -29,4 +31,6 @@ const destinationSchema = new mongoose.Schema({
   
 export const Destination = mongoose.model('destinations', destinationSchema);
 export const getDestinations = () => Destination.find()
-export const createDestination = (newDestination) => new Destination(...newDestination).save()
+export const createDestination = (newDestination) => new Destination(newDestination).save();
+export const deleteDestination = (id) => Destination.findByIdAndDelete(id)
+export const updateDestination = (id, newDestination) => Destination.findByIdAndUpdate(id, newDestination)
