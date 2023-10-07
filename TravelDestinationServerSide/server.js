34a9c153cd1258f937cd30d35dab513d48938023
Pavel Andrei <pavelhandup@gmail.com>
2023-10-07 15:55:53 +0300

    added login api

--- TravelDestinationServerSide/server.js
+++ TravelDestinationServerSide/server.js
@@ -57,7 +57,7 @@ app.post(
 );
 
 app.post("/register", (req, res) => { 
-	User.register(new User({ email: req.body.email, username: req.body.email }), req.body.password, function (err, user) { 
+	User.register(new User({ username: req.body.email }), req.body.password, function (err, user) { 
 		if (err) { 
 			res.json({ success: false, message: "Your account could not be saved. Error: " + err }); 
 		} 
@@ -73,6 +73,32 @@ app.post("/register", (req, res) => {
 		} 
 	}); 
 }); 
+
+app.post("/login", function (req, res) { 
+	if (!req.body.username) { 
+		res.json({ success: false, message: "Email was not given" }) 
+	} 
+	else if (!req.body.password) { 
+		res.json({ success: false, message: "Password was not given" }) 
+	} 
+	else { 
+		passport.authenticate("local", function (err, user, info) { 
+			if (err) { 
+				res.json({ success: false, message: err }); 
+			} 
+			else { 
+				if (!user) { 
+					res.json({ success: false, message: "username or password incorrect" }); 
+				} 
+				else { 
+					const token = jwt.sign({ userId: user._id, username: user.username }, secretkey, { expiresIn: "24h" }); 
+					res.json({ success: true, message: "Authentication successful", token: token }); 
+				} 
+			} 
+		})(req, res); 
+	} 
+}); 
+
 
 
 // DELETE localhost:40000/destinations/:id => delete a destination
